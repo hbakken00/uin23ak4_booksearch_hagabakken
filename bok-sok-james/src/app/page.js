6f753fb3ee26cf6afcbaa671603a779cfda5ba87
hbakken00 <nikhaug@outlook.com>
2024-04-01 18:50:18 +0200

    Oppretting av next prosjekt
    Med create-next-app

---
+++ bok-sok-james/src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+ 
+
+ <section>
+  <h2> James Bond Bøker </h2>
+  
+
+ </section>
+
+  );
+}
